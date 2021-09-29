265d88f4d2de4b995f89658e29fd1d31380edceb
vicmaburrito <vmaldaraca@gmail.com>
2021-09-29 17:47:02 -0500

    Create first objects & custom index.html

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+const button = document.querySelector('button');
+class Todo {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
+
+const todos = [
+  new Todo('Hello world 1', false, 0),
+  new Todo('Hello world 2', false, 1),
+  new Todo('Hello world 5', false, 4),
+  new Todo('Hello world 3', false, 2),
+  new Todo('Hello world 4', false, 3),
+];
