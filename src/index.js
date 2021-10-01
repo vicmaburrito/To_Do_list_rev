8d0e6ba20def2bca90d9313275b82134e774d450
vicmaburrito <vmaldaraca@gmail.com>
2021-10-01 16:13:31 -0500

    Complete app

--- src/index.js
+++ src/index.js
@@ -27,7 +27,22 @@ function createTodoItem(todo) {
       </span>
     </div>
     <hr>`;
+
   return li;
+}
+
+function ReplaceTodoItem(todo) {
+  const html = `
+  <div>
+    <input type="checkbox" class="checkbox" 
+    ${todo.completed ? 'checked' : ''}>
+    <span>${todo.description}</span>
+  </div>
+  <span class="material-icons edit-icon" style=" cursor: pointer">
+      more_vert
+  </span>
+    `;
+  return html;
 }
 
 function addTodoItem(todo) {
@@ -46,12 +61,103 @@ function saveTodosLocally() {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-function addEventsToCheckboxes() {
+function ReplaceTodoItemForCompletedTask(todo) {
+  const html = `
+  
+  <div>
+  <span class="material-icons edit-icon" style=" cursor: pointer; color: green">
+      done
+  </span>
+    <strike><span>${todo.description}</span></strike>
+  </div>
+  <span class="material-icons edit-icon" style=" cursor: pointer">
+      more_vert
+  </span>
+    `;
+
+  return html;
+}
+
+function changeElementToCompleted(index) {
+  update(todos[index]);
+  saveTodosLocally();
+  if (todos[index].completed) {
+    const completedElement = ReplaceTodoItemForCompletedTask(todos[index]);
+    const todoElements = document.querySelectorAll('.todo-element');
+    todoElements[index].innerHTML = completedElement;
+  }
+}
+
+function addEventsToCheckboxes(recievedIndex) {
   const checkboxes = document.querySelectorAll('.checkbox');
+
   checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener('change', () => {
-      update(todos[index]);
-      saveTodosLocally();
+    if (recievedIndex) {
+      if (recievedIndex === index) {
+        checkbox.addEventListener('change', () => {
+          changeElementToCompleted(index);
+        });
+      }
+    } else {
+      checkbox.addEventListener('change', () => {
+        changeElementToCompleted(index);
+      });
+    }
+  });
+}
+
+function addEventsToEditIcons() {
+  const editIcons = document.querySelectorAll('.edit-icon');
+  const todoElements = document.querySelectorAll('.todo-element');
+
+  todos.forEach((todo, index) => {
+    editIcons[index].addEventListener('click', () => {
+      const div = document.createElement('div');
+      div.classList.add('flex', 'todo-element');
+      div.style.backgroundColor = '#FFFBAE';
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.classList.add('checkbox');
+      checkbox.checked = todo.completed;
+
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.classList.add('edit-input');
+      input.value = todo.description;
+      input.style.backgroundColor = 'transparent';
+
+      const span = document.createElement('span');
+      span.classList.add('material-icons', 'edit-icon');
+      span.style.marginLeft = 'auto';
+      span.style.cursor = 'pointer';
+      span.innerHTML = 'delete';
+
+      div.appendChild(checkbox);
+      div.appendChild(input);
+      div.appendChild(span);
+
+      todoElements[index].replaceWith(div);
+
+      input.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+          const todo = todos[index];
+          todo.description = input.value;
+          updateTodo(todo, todos[index]);
+          const html = ReplaceTodoItem(todo);
+          div.innerHTML = html;
+          addEventsToEditIcons();
+          saveTodosLocally();
+          div.style.backgroundColor = 'white';
+        }
+      });
+
+      span.addEventListener('click', () => {
+        saveTodosLocally();
+        destroyTodo(todo, todos);
+        div.parentElement.remove();
+        saveTodosLocally();
+      });
     });
   });
 }
@@ -63,4 +169,39 @@ window.addEventListener('load', () => {
   }
   populate();
   addEventsToCheckboxes();
+  addEventsToEditIcons();
+});
+
+function addEventListenerToInput() {
+  const input = document.querySelector('#input');
+  input.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+      const todo = new Todo(input.value, false, todos.length + 1);
+      createTodo(todo, todos);
+      addTodoItem(todo);
+      saveTodosLocally();
+      input.value = '';
+      addEventsToEditIcons(todos.length);
+      addEventsToCheckboxes(todos.length - 1);
+    }
+  });
+}
+
+addEventListenerToInput();
+
+button.addEventListener('click', () => {
+  const todoElements = document.querySelectorAll('.todo-element');
+  const removedTodos = [];
+  for (let i = 0; i < todos.length; i += 1) {
+    if (todos[i].completed === true) {
+      removedTodos.push(todos[i]);
+      todoElements[i].parentNode.remove();
+    }
+  }
+
+  removedTodos.forEach((todo) => {
+    destroyTodo(todo, todos);
+  });
+
+  saveTodosLocally();
 });
